a7a9753c1e1ca16c289e48823c2b7b1c4d441c7e
Aigerim2024 <llc.sfdev@gmail.com>
2024-07-27 10:58:19 -0500

    updated

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "aigerim" {
+resource "aws_key_pair" "aigerimka" {
   key_name   = "terraform7"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK24KOC34VLih/r7t6MMD5Zm5glvKj6omHkDvP4Zds4K aigerimkadyrbai@Aigerims-MacBook-Pro.local"
 
