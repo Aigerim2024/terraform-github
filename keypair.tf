7c52b4be444d901c26a61b9598b514ab56acee2b
Aigerim2024 <llc.sfdev@gmail.com>
2024-07-27 09:16:24 -0500

    keypair

---
+++ keypair.tf
@@ -0,0 +1,11 @@
+resource "aws_key_pair" "aigerim" {
+  key_name   = "terraform7"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK24KOC34VLih/r7t6MMD5Zm5glvKj6omHkDvP4Zds4K aigerimkadyrbai@Aigerims-MacBook-Pro.local"
+
+}
+
+variable "key_name" {
+    type = string 
+    default = "terraformgithun"
+  
+}
